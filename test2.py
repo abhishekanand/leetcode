6c0dbe80e360240da3391f960b842b7781e4bdd4
Abhishek Anand <anand1@uw.edu>
2018-06-17 23:53:18 -0700

    autopep8 --in-place --aggressive --aggressive <filename>

--- test2.py
+++ test2.py
@@ -5,25 +5,26 @@ Program DocString
 """
 import sys
 
+
 def indexValue(i_array, target):
     """
-     Function Docstring 
+     Function Docstring
     """
-    if(len(i_array)<= 2):
+    if(len(i_array) <= 2):
         return False
     else:
         for i in range(len(i_array)):
             first_index = i
-            j = i+1
+            j = i + 1
             for j in range(len(i_array)):
-                if i_array[i]+i_array[j] == target:
+                if i_array[i] + i_array[j] == target:
                     second_index = j
-                    return [first_index,second_index]
+                    return [first_index, second_index]
     return False
 
 
 if __name__ == "__main__":
-    inputArray = [2,7,10,11]
+    inputArray = [2, 7, 10, 11]
     target = 13
-    out = indexValue(inputArray,target)
+    out = indexValue(inputArray, target)
     print(out)
